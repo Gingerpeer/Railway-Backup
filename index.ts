005a23f7224ad34dee5d8c3ae900a34e9ffd70ad
Pierre Daniel van Zyl <pierrevanzyl777@gmail.com>
2023-11-07 17:55:18 +0200

    made dbBackups for both ag and aquaman

--- index.ts
+++ index.ts
@@ -1,30 +1,26 @@
 import { spawn } from "bun";
-
-
-async function backupPostgresqlDB() {
+  // avantgarde db
+  const today = await new Date().toDateString().replace(/\s/g, '')
   const user = Bun.env.PGUSER;
   const host = Bun.env.PGHOST;
   const port = Bun.env.PGPORT;
   const db = Bun.env.PGDATABASE;
-  const fileName = Bun.env.YOUR_FILENAME_HERE;
+  const fileName = `${Bun.env.YOUR_FILENAME_HERE}${today}`;
   const pass = Bun.env.PGPASSWORD;
+  // aquaman db
+  const aquaman_user = Bun.env.AQUAMAN_PGUSER;
+  const aquaman_host = Bun.env.AQUAMAN_PGHOST;
+  const aquaman_port = Bun.env.AQUAMAN_PGPORT;
+  const aquaman_db = Bun.env.AQUAMAN_PGDATABASE;
+  const aquaman_fileName = `${Bun.env.AQUAMAN_YOUR_FILENAME_HERE}${today}`;
+  const aquaman_pass = Bun.env.AQUAMAN_PGPASSWORD;
 
-  let start = false;
-  console.log("Starting Backup")
-  const child = spawn(['bash', './backup.sh', user, host, port, db, fileName, pass]);
-
-  const res = await new Response(child.stdout);
+async function backupPostgresqlDB() {
   
-  const ok = await res.ok;
-
-  if(ok){
-    start = ok
-  }
-
-  if(start){
-    child.kill()
-    console.log("Backup Completed")
-  }
+    console.log("Starting Backup")
+    await spawn(['bash', './backup.sh', user, host, port, db, fileName, pass]);
+    spawn(['bash', './backup.sh', aquaman_user, aquaman_host, aquaman_port, aquaman_db, aquaman_fileName, aquaman_pass]);
+  
 }
 
-backupPostgresqlDB()  
+backupPostgresqlDB()
